041363ce05db0ea652781f8e985a876a7b85ce2b
Antonin <antonin.poche@irt-saintexupery.com>
2026-06-29 16:52:52 +0200

    fix sync env breaking

--- sync-jz.sh
+++ sync-jz.sh
@@ -5,7 +5,7 @@ rsync -az --delete --partial \
   -e "ssh -o ServerAliveInterval=30 -o ServerAliveCountMax=6" \
   --exclude ".git/" \
   --exclude "__pycache__/" \
-  --exclude ".venv/" \
+  --exclude ".venv" \
   --exclude ".pytest_cache/" \
   --exclude ".ruff_cache/" \
   --exclude "data/" \
